64bd5b893ee60827e64232542c2e08bf421cc877
Rudra Lakhtariya <rudrapvt07@gmail.com>
2026-04-08 17:55:36 +0530

    demo hosting 10

--- fitmind_frontend/lib/core/constants/api_constants.dart
+++ fitmind_frontend/lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // Toggle this for Local vs Production
-  static const bool isProduction = false;
+  static const bool isProduction = true;
 
   // Local development URL (10.0.2.2 is the localhost address for Android emulators)
   static const String localBaseUrl = 'http://10.0.2.2:8000/api/v1';
